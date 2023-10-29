410d4793c4087e1d8a0911f74c6a1be226663430
xmasuhai <xmasuhai@163.com>
2023-10-29 20:35:49 +0800

    使用 styled-components 写一个 div 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import 'style/App.scss'
+import {Button} from 'components/Button'
+import styled from 'styled-components'
+
+const Box = styled.div`
+  border: 1px solid darkgrey;
+  min-height: 300px;
+  min-width: 200px;
+  margin: 20px;
+  box-shadow: 0 0 3px 2px rgba(0,0,0,0.5);
+`
 
 function App() {
   return (
     <div className="App">
-      Hi
+      <Box>
+        <Button>Hi</Button>
+      </Box>
     </div>
   )
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  color: red;
+  border: none;
+  &:hover { border: 1px solid ;}
+`
