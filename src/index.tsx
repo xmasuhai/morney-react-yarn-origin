7d46963ac5fbe7783959162be87b18f2da4f85c7
xmasuhai <xmasuhai@163.com>
2023-10-29 19:15:43 +0800

    使用 styled-components @types/styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
 
 const div = document.getElementById('root')
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   div
-);
+)
