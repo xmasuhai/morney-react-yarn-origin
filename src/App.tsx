b944a892fe6d5e4d4b0151d97b0693613552ade2
xmasuhai <xmasuhai@163.com>
2023-12-04 06:48:53 +0800

    :sparkles: 更改路由，配置默认路由和404路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
 /**
 * @Description 根组件
 * */
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom'
+
+export const App = () => {
   return (
-    <div className="App">
-      Hello
-    </div>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistiscs">统计页</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistiscs">
+            <Statistics />
+          </Route>
+          {/* 注意重定向组件的位置，放在Switch里面 */}
+          <Redirect exact from="/" to="money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+
+function NoMatch() {
+  return <h2>404</h2>;
+}
