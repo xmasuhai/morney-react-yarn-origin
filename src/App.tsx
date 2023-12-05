ec8550e1001a1454825d5a75fad4c65d07b7f6dd
xmasuhai <xmasuhai@163.com>
2023-12-06 06:38:58 +0800

    导航栏布局 修复依赖警告

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,59 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const Nav = styled.nav`
+  border: 1px solid cyan;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+
+`
 
 export const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
+      <Wrapper>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            {/* 注意重定向组件的位置，放在Switch里面 */}
+            <Redirect exact from="/" to="money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+
+          </Switch>
+        </Main>
+
+        <Nav>
           <ul>
             <li>
               <Link to="/tags">标签页</Link>
@@ -25,28 +72,9 @@ export const App = () => {
               <Link to="/statistics">统计页</Link>
             </li>
           </ul>
-        </nav>
+        </Nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          {/* 注意重定向组件的位置，放在Switch里面 */}
-          <Redirect exact from="/" to="money" />
-          <Route path="*">
-            <NoMatch />
-          </Route>
-
-        </Switch>
-      </div>
+      </Wrapper>
     </Router>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { App } from './App'
 import './index.scss'
-import {App} from './App'
 
 const div = document.getElementById('root')
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   div
 )
