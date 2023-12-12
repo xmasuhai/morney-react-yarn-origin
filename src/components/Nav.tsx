f605e4283bda8d36e599f99cf4fe926c9daff7e3
xmasuhai <xmasuhai@163.com>
2023-12-13 06:17:57 +0800

    使用 svg 引入三个图标，设置基本样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,21 @@
 import styled from 'styled-components'
 import {Link} from 'react-router-dom'
-import x from 'assets/icons/edit.svg'
+/*
+// eslint-disable-next-line
+import tag from 'assets/icons/bills.svg' // svgo-loader -> svg-sprite-loader
+// eslint-disable-next-line
+import edit from 'assets/icons/edit.svg'
+// eslint-disable-next-line
+import statistics from 'assets/icons/statistics.svg'
+// console.log(tag, edit, statistics,)
+import 'assets/icons/bills.svg'
+import 'assets/icons/edit.svg'
+import 'assets/icons/statistics.svg'
+*/
 
-console.log(x)
+require('assets/icons/bills.svg')
+require('assets/icons/edit.svg')
+require('assets/icons/statistics.svg')
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -12,7 +25,17 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
+      
     }
   }
 `
@@ -21,13 +44,21 @@ export const Nav = () => (
   <NavWrapper>
     <ul>
       <li>
-        <img src={x} alt="" />
+        <svg className="icon">
+          <use xlinkHref="#bills"></use>
+        </svg>
         <Link to="/tags">标签页</Link>
       </li>
       <li>
+        <svg className="icon">
+          <use xlinkHref="#edit"></use>
+        </svg>
         <Link to="/money">记账页</Link>
       </li>
       <li>
+        <svg className="icon">
+          <use xlinkHref="#statistics"></use>
+        </svg>
         <Link to="/statistics">统计页</Link>
       </li>
     </ul>
