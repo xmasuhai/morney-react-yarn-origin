425c3e703387af58209df9a7d8bbdf26a600c5b2
xmasuhai <xmasuhai@163.com>
2023-12-17 14:02:00 +0800

    Money.tsx 之 备注 UI 完成： Update styling for Nav and NotesSection
    The Nav component background color has been updated to white for better visibility. For the NotesSection in Money component, substantial styling updates were made including background color, padding, font size and layout adjustments. Moreover, the input field now includes a placeholder for better user guidance.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom'
 import { Icon } from './Icon'
 
 const NavWrapper = styled.nav`
+  background-color: #fff;
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -29,7 +29,27 @@ const TagsSection = styled.section`
   }
 `
 
-const NotesSection = styled.section``
+const NotesSection = styled.section`
+  background-color: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+
+  > label {
+    display: flex;
+    align-items: center;
+
+    > span { margin-right: 16px; white-space: nowrap; }
+
+    > input { 
+      display: block; 
+      width: 100%;
+      height: 72px;
+      background-color: transparent;
+      
+    }
+  }
+
+`
 
 const CategorySection = styled.section``
 
@@ -51,7 +71,7 @@ export function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
