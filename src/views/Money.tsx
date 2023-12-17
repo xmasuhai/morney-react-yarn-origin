7e991e635c8dff82393315960ba1528bedd5a991
xmasuhai <xmasuhai@163.com>
2023-12-18 05:26:07 +0800

    :lipstick: Money.tsx 之 类型 UI 完成： Update styling for CategorySection in Money.tsx

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -51,7 +51,35 @@ const NotesSection = styled.section`
 
 `
 
-const CategorySection = styled.section``
+const CategorySection = styled.section`
+  font-size: 24px;
+  
+  > ul {
+    display: flex;
+    justify-content: center;
+    background-color: #c4c4c4;
+    position: relative;
+    
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background-color: #333;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+      }
+      
+    }
+  }
+`
 
 const NumberPadSection = styled.section``
 
@@ -76,7 +104,7 @@ export function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
