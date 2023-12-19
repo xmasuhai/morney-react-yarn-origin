ec6704371e6462930dcc58f4f8c809e3a94e936e
xmasuhai <xmasuhai@163.com>
2023-12-20 06:38:51 +0800

    Money.tsx 抽离模块组件；完成标签的选择和新增功能与样式 Refactor components in Money view
    The code for the components TagsSection, NotesSection, NumberPadSection, and CategorySection were extracted from Money.tsx into individual component files. This enhances the organization and modularity of the codebase, improving the maintainability and readability of the code.

---
+++ src/components/money/CategorySection.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+/**
+ * 收支切换 模块
+ * */
+export const CategorySection = styled.section`
+  font-size: 24px;
+  
+  > ul {
+    display: flex;
+    justify-content: center;
+    background-color: #c4c4c4;
+    position: relative;
+    
+    > li {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+      position: relative;
+      
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 3px;
+        background-color: #333;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+      }
+      
+    }
+  }
+`
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,145 +1,10 @@
 import React from 'react'
-import {Layout} from '../components/Layout'
+import {Layout} from 'components/Layout'
 import styled from 'styled-components'
-
-const TagsSection = styled.section`
-  // parent flex
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-  
-  background-color: #fff;
-  padding: 12px 16px;
-
-  > ol {
-    margin: 0 -12px;
-
-    > li {
-      background-color: #d9d9d9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-    }
-  }
-
-  > button {
-    background-color: unset;
-    padding: 2px 4px;
-    margin-top: 8px;
-    border-bottom: 1px solid #333;
-    color: #666;
-
-  }
-`
-
-const NotesSection = styled.section`
-  background-color: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
-
-  > label {
-    display: flex;
-    align-items: center;
-
-    > span { margin-right: 16px; white-space: nowrap; }
-
-    > input { 
-      display: block; 
-      width: 100%;
-      height: 72px;
-      background-color: transparent;
-      
-    }
-  }
-
-`
-
-const CategorySection = styled.section`
-  font-size: 24px;
-  
-  > ul {
-    display: flex;
-    justify-content: center;
-    background-color: #c4c4c4;
-    position: relative;
-    
-    > li {
-      width: 50%;
-      text-align: center;
-      padding: 16px 0;
-      position: relative;
-      
-      &.selected::after {
-        content: '';
-        display: block;
-        height: 3px;
-        background-color: #333;
-        position: absolute;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-      }
-      
-    }
-  }
-`
-
-const NumberPadSection = styled.section`
-  display: flex;
-  flex-direction: column;
-
-  > .output {
-    background-color: #fff;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-
-  > .pad {
-    
-    > button {
-      font-size: 18px;
-      float: left;
-      width: 25%;
-      height: 64px;
-      
-      &.ok {
-        float: right;
-        height: 128px;
-      }
-      &.zero {width: 50%;}
-      
-      &:nth-child(1) {background-color: #f2f2f2;}
-      
-      &:nth-child(2),
-      &:nth-child(5) {background-color: #e0e0e0;}
-      
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9) {background-color: #d3d3d3;}
-      
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10) {background-color: #c1c1c1;}
-
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13) {background-color: #b8b8b8;}
-
-      &:nth-child(12) {background-color: #9a9a9a;}
-      &:nth-child(14) {background-color: #a9a9a9;}
-      
-    }
-    
-  }
-`
+import {TagsSection} from 'components/money/TagsSection'
+import {NotesSection} from 'components/money/NotesSection'
+import {NumberPadSection} from 'components/money/NumberPadSection'
+import {CategorySection} from 'components/money/CategorySection'
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -150,15 +15,7 @@ const MyLayout = styled(Layout)`
 export function Money() {
   return  (
     <MyLayout>
-      <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
+      <TagsSection />
       <NotesSection>
         <label>
           <span>备注</span>
