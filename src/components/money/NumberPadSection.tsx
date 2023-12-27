deb047477dc4ebb3543535f070272f840cb5ecaa
xmasuhai <xmasuhai@163.com>
2023-12-28 06:46:37 +0800

    :construction: numberPad 功能 wip(working in progress) Update packages and enhance NumberPad functionality
    Updated "@babel/core" and "semver" packages to newer versions in package.json. The NumberPad component in NumberPadSection.tsx has been enhanced with a useState hook to manage output state and introduced switch cases in the onClickButtonWrapper function to handle button clicks.

--- src/components/money/NumberPadSection.tsx
+++ src/components/money/NumberPadSection.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import React from 'react'
-import {generateNthChildStyles} from '../../styled-components-helpers/generateNthChildStyles'
+import React, {useState} from 'react'
+import {generateNthChildStyles} from 'styled-components-helpers/generateNthChildStyles'
 
 const NumberPadStyled = styled.section`
   display: flex;
@@ -17,23 +17,24 @@ const NumberPadStyled = styled.section`
   }
 
   > .pad {
-    
+
     > button {
       font-size: 18px;
       float: left;
       width: 25%;
       height: 64px;
-      
+
       &.ok {
         float: right;
         height: 128px;
       }
+
       &.zero {width: 50%;}
-      
+
       ${generateNthChildStyles()}
-      
+
     }
-    
+
   }
 `
 
@@ -41,11 +42,50 @@ const NumberPadStyled = styled.section`
  * 数字键盘
  * */
 export const NumberPad = () => {
+  const [output, setOutput] = useState('0')
+
+  /* 事件代理 键盘点击事件 */
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if(!text) {return}
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '.':
+        if(output === '0') {
+          setOutput(text)
+        } else {
+          setOutput(output + text)
+        }
+        break
+      case 'OK':
+        console.log('确认')
+        break
+      case '删除':
+        console.log('删除')
+        break
+      case '清空':
+        console.log('清空')
+        break
+
+    }
+
+  }
 
   return (
     <NumberPadStyled>
-      <div className="output">100</div>
-      <div className="pad clear-fix">
+      <div className="output">{output}</div>
+      <div
+        className="pad clear-fix"
+        onClick={onClickButtonWrapper}>
         <button>1</button>
         <button>2</button>
         <button>3</button>
