fa932c0e6b9350213ac634b20789519e658df06f
xmasuhai <xmasuhai@163.com>
2024-01-01 21:49:24 +0800

    :bug: fix bug when input . Implement useState for output in NumberPadSection
    The previous constant output has been replaced by a useState function to manage output value dynamically within the NumberPadSection component. This change improves the overall efficiency, as the output value will now be updated through the state. Additionally, genOutput usage has also been updated to align with this state change.

--- src/components/money/NumberPadSection.tsx
+++ src/components/money/NumberPadSection.tsx
@@ -1,4 +1,4 @@
-import React, {FC,} from 'react'
+import React, {FC, useState,} from 'react'
 import {NumberPadStyled} from './styled/NumberPadStyled'
 import {InputTextString, genOutput} from './helpers/genOutput'
 
@@ -18,13 +18,14 @@ export const NumberPad: FC<Props> = (
     onConfirm,
   }
 ) => {
-  const output = `${amountValue}`
+  const [outputValue, setOutputValue] = useState(`${amountValue}`)
 
   /* 统一处理 setOutput */
   const setOutput = (output: string) => {
-    if(output.length > 16) {onAmountChange(parseFloat(output.slice(0, 16)))} else if(output.length === 0) {onAmountChange(0)}
-    // TODO fix bug when input .
-    else {onAmountChange(parseFloat(output))}
+    let newOutput: string
+    if(output.length > 16) {newOutput = output.slice(0, 16)} else if(output.length === 0) {newOutput = '0'} else { newOutput = output }
+    setOutputValue(newOutput)
+    onAmountChange(parseFloat(newOutput))
   }
 
   /* 事件代理 键盘点击事件 */
@@ -40,14 +41,14 @@ export const NumberPad: FC<Props> = (
 
     // TODO 使用类型枚举优化
     if('0123456789.'.split('').concat(['删除', '清空',]).includes(text)) {
-      setOutput(genOutput(text as InputTextString , output))
+      setOutput(genOutput(text as InputTextString, outputValue))
     }
 
   }
 
   return (
     <NumberPadStyled>
-      <div className="output">{output}</div>
+      <div className="output">{outputValue}</div>
       <div
         className="pad clear-fix"
         onClick={onClickButtonWrapper}>
