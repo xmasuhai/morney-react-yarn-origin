d6d59de779219db2faf129064e8516a1ea50b899
xmasuhai <xmasuhai@163.com>
2024-01-03 06:59:01 +0800

    Update initial tags state and display them in Tags view
    This change modifies useTags.tsx to set the initial tags state as actual category names instead of placeholders. Additionally, Tags.tsx has been adjusted to display these tags as an ordered list, enhancing their visibility on the page.

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import {Layout} from '../components/Layout'
+import {Layout} from 'components/Layout'
+import {useTags} from 'components/money/hooks/useTags'
 
 // 标签页面
 export function Tags() {
+  const {tags, setTags}= useTags()
+
+
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <ol>
+        {tags.map(tag => (<li key={tag}>{tag}</li>))}
+      </ol>
     </Layout>
   )
 }
