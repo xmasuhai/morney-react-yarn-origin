f800ad4782199a483f715521afe2c865d6e4eb8f
xmasuhai <xmasuhai@163.com>
2025-02-19 16:44:14 +0800

    fix(src/hooks): 提取默认标签数组到外部变量
    - 将默认标签数组从 useTags函数内部提取到外部变量 defaultTags，修复调用useTags重复递增id的问题

--- src/hooks/useTags.ts
+++ src/hooks/useTags.ts
@@ -3,23 +3,23 @@ import {createId} from 'lib/createId'
 
 export type TagObj = {id: number, name: string}
 
+const defaultTags: TagObj[] = [
+  {id: createId(), name: '衣'},
+  {id: createId(), name: '食'},
+  {id: createId(), name: '住'},
+  {id: createId(), name: '行'},
+]
+
 /**
  * @Description: 存储所有标签数据
  * @Author: XuShuai
  * @Date: 2024-01-03 06:52:17
  * @LastEditors: XuShuai
- * @LastEditTime: 2025-02-19 16:10:28
+ * @LastEditTime: 2025-02-19 16:34:13
  * @FilePath: src/hooks/useTags.ts
  */
 export const useTags = () => {
-  const [tags, setTags] = useState<TagObj[]>(
-    [
-      {id: createId(), name: '衣'},
-      {id: createId(), name: '食'},
-      {id: createId(), name: '住'},
-      {id: createId(), name: '行'},
-    ]
-  )
+  const [tags, setTags] = useState<TagObj[]>(defaultTags)
 
   return {
     tags,
